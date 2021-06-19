2b117eb8f17eb7a525802f73ee005a64cc6b73b5
Ai Uragaki <ai.uragaki.nz@gmail.com>
2021-06-19 19:26:39 +1200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _10ArrayShowingOddNumbers
+{
+    class Program
+    {
+        static void Main(string[] args) // Declare and initialize array of 10 integers. Then print only the odd number elements
+        {
+            
+            //declaring array of integers
+            int[] balance = new int[10];
+            //reading the elements
+                int[] marks = new int[10] { 94, 283, 850, 274, 3764, 98, 34, 25, 3, 20 };
+            
+           
+
+            for (int i = 0; i < 10; i++)
+                
+//print only the odd number
+            if (marks[i] % 2 == 1)
+               
+
+                Console.WriteLine(marks[i] + "is odd number");
+                
+           
+         
+
+
+
+
+
+        }
+    }
+}
